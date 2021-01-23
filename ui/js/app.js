be711c5ae50455c9e01daf2ff85ce67db05e7399
therajatrai <61858752+therajatrai@users.noreply.github.com>
2021-01-23 12:14:10 +0530

    Update app.js

--- ui/js/app.js
+++ ui/js/app.js
@@ -36,7 +36,7 @@ $('#errorbox').hide()
   var aadhaar_no_phone_no = {
   	"7382537xxxxx": "915801xxxx",
   	"300000000000": "7276xxxxxx",
-	"<replace your aadhaar no here>": "<your phone number>",
+	"699559457014":"9911930964,
   }
 
 
